188709ba79fc5cf2f2c0fff8caa518e6afa567a5
Roman Antonyk <roman.antonyk.03@gmail.com>
2022-11-07 13:10:10 +0200

    Add custom input

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Input = ({value, onChange, type}) => {
+  return (
+        <input type={type}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+         focus:ring-blue-500 focus:border-blue-500 block w-full p-1
+         dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+         dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+         value={value} onChange ={onChange}
+
+         required/>
+  )
+}
+
+export default Input
